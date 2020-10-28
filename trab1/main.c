b504889ff405bbe94df5aaed31fae87a74bef5d9
Danilo <loklosgames@gmail.com>
2020-10-28 12:18:36 -0700

    definicao da main

--- trab1/main.c
+++ trab1/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include "wiked.h"
 
-int main(char * argv, int argc){
+int main(char * argv[], int argc){
+    FILE * entrada = fopen(argv[1],"r");
+    WikED * wiki = inicializaWikED();
     
+    leituraEntrada(entrada, wiki);
+
+    fclose(argv[1]);
+
+    return 0;
 }
