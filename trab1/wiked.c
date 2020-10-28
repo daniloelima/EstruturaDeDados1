2820b2b41d9fa421a6cba6f1c7095a8d4ec730e4
Danilo <loklosgames@gmail.com>
2020-10-28 12:28:07 -0700

    leitura arquivo de entrada

--- trab1/wiked.c
+++ trab1/wiked.c
@@ -1,8 +1,85 @@
 #include "wiked.h"
 #include "pagina.h"
 #include "editor.h"
+#define MAX_TAM 20
 
+//sentinela que indica as 2 listas (pagina e editores)
 struct wiked{
-    CelPagina * ppags; //ponteiro primeira celulapag
-    CelEditor * pedits; //ponteiro primeira celulaeditor
+    listaPagina * pripagi; //ponteiro primeira celulapag
+    listaPagina * ultpagi; //ponteiro ultima
+    listaEditor * priedit; //ponteiro primeira celulaeditor
+    listaEditor * ultedit; //ponteiro ultima
 };
+
+WikED * inicializaWikED(){
+    WikED * wiki = (WikED*) malloc (sizeof(struct wiked));
+    
+    wiki->pripagi = NULL;
+    wiki->ultpagi = NULL;
+    wiki->priedit = NULL;
+    wiki->ultedit = NULL;
+
+    return wiki;
+}
+
+void leituraEntrada(FILE * entrada, WikED * wiki){
+    char funcao[MAX_TAM];
+    while(fscanf(entrada, "%s ", funcao) == 1 && !strcmp(funcao, "FIM")){
+        //chama a funcao "leituraFuncao" que identifica qual a funcao
+        leituraFuncao(entrada, wiki, funcao);
+    }
+
+    liberaWikED(wiki);
+}
+
+void leituraFuncao(FILE * entrada, WikED * wiki, char * funcao){
+    if(!strcmp(funcao, "INSEREPAGINA")){
+        char nomepagina[MAX_TAM], arquivopagina[MAX_TAM];
+        fscanf(entrada, " %s %s ", nomepagina, arquivopagina);
+        //chama a funcao que insere pagina na lista da wiki
+    }
+    else if(!strcmp(funcao, "RETIRAPAGINA")){
+        char nomepagina[MAX_TAM];
+        fscanf(entrada, " %s ", nomepagina);
+        //chama a funcao que retira a pagina na lista da wiki
+    }
+    else if(!strcmp(funcao, "INSEREEDITOR")){
+        char nomeeditor[MAX_TAM];
+        fscanf(entrada, " %s ", nomeeditor);
+        //chama a funcao que insere um editor na lista da wiki
+    }
+    else if(!strcmp(funcao, "RETIRAEDITOR")){
+        char nomeeditor[MAX_TAM];
+        fscanf(entrada, " %s ", nomeeditor);
+        //
+    }
+    else if(!strcmp(funcao, "INSERECONTRIBUICAO")){
+        char  nomepagina[MAX_TAM], nomeeditor[MAX_TAM], arquivocontribuicao[MAX_TAM];
+        fscanf(entrada, " %s ", nomeeditor);
+        //
+    }
+    else if(!strcmp(funcao, "RETIRACONTRIBUICAO")){
+        char  nomepagina[MAX_TAM], nomeeditor[MAX_TAM], arquivocontribuicao[MAX_TAM];
+        fscanf(entrada, " %s ", nomeeditor);
+        //
+    }
+    else if(!strcmp(funcao, "INSERELINK")){
+        char nomepaginaOrigem[MAX_TAM], nomepaginaDestino[MAX_TAM];
+        fscanf(entrada, " %s %s ", nomepaginaOrigem, nomepaginaDestino);
+    }
+    else if(!strcmp(funcao, "RETIRALINK")){
+        char nomepaginaOrigem[MAX_TAM], nomepaginaDestino[MAX_TAM];
+        fscanf(entrada, " %s %s ", nomepaginaOrigem, nomepaginaDestino);
+    }
+    else if(!strcmp(funcao, "CAMINHO")){
+        char nomepaginaOrigem[MAX_TAM], nomepaginaDestino[MAX_TAM];
+        fscanf(entrada, " %s %s ", nomepaginaOrigem, nomepaginaDestino);
+    }
+    else if(!strcmp(funcao, "IMPRIMEWIKED")){
+        // imprime a lista
+    }
+    else{
+        printf("\nfunção não encontrada! saindo do programa...\n");
+        exit(1);
+    }
+}
