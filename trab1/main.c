cea28e00ba4121fa64de8733e03f9327c6b0fa27
Danilo <loklosgames@gmail.com>
2020-11-17 05:07:52 -0800

    reestruturar a main

--- trab1/main.c
+++ trab1/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include "wiked.h"
+#define MAX_TAM 20
 
 int main(char * argv[], int argc){
     FILE * entrada = fopen(argv[1],"r");
+    FILE * log = fopen("log.txt", "w");
+
     WikED * wiki = inicializaWikED();
     
-    leituraEntrada(entrada, wiki);
-    
+    //leituraEntrada(entrada, wiki);
+    char funcao[MAX_TAM];
+
+    while(fscanf(entrada, "%s ", funcao) == 1 && strcmp(funcao, "FIM")){
+        leituraFuncao(entrada, log, wiki, funcao);
+    }
+
     liberaWiki(wiki);
     fclose(argv[1]);
 
